948cd7bbcbd17c02d28009296732ccb8f3cd26f1
Summer <1345635800@qq.com>
2016-09-02 16:50:51 +0800

    Activity

---
+++ app/src/main/java/com/dongdao/a/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.dongdao.a.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dongdao.a.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
